54f84a0b2d3456d7d71accf739f74f24e3044676
Nziranziza <nziranzizadaniel@gmail.com>
2022-06-03 18:11:38 +0200

    add pg

--- src/db/config.ts
+++ src/db/config.ts
@@ -6,10 +6,25 @@ const dbUser = process.env.DB_USER as string;
 const dbHost = process.env.DB_HOST as string;
 const dbDriver = process.env.DB_DRIVER as Dialect;
 const dbPassword = process.env.DB_PASSWORD as string;
+const databaseUrl = process.env.DATABASE_URL as string;
 
-const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
-  host: dbHost,
-  dialect: dbDriver,
-});
+let sequelizeConnection: Sequelize;
+
+if(databaseUrl) {
+  sequelizeConnection = new Sequelize(databaseUrl, {
+    "logging" : false,
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
+  });
+} else {
+  sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    dialect: dbDriver,
+  });
+}
 
 export default sequelizeConnection;
